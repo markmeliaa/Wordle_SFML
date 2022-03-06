e769a0a43b4669793937b35844344a85ebbca400
Marc Alcón Meliá <marcaalconmelia@gmail.com>
2022-03-06 20:09:02 +0100

    Write PuzzleLetter Class

--- WordleSFML/WordleSFML/PuzzleLetter.cpp
+++ WordleSFML/WordleSFML/PuzzleLetter.cpp
@@ -1,2 +1,63 @@
 #include "PuzzleLetter.h"
 
+PuzzleLetter::PuzzleLetter(const sf::IntRect& bounds, const sf::Font& font)
+	: _bounds(bounds), _letter(' '),
+	_background(sf::Vector2f(_bounds.width - 2, _bounds.height - 2)),
+	_textVisual('A', font, 30)
+{
+	_textVisual.setFillColor(sf::Color::White);
+	_textVisual.setStyle(sf::Text::Bold);
+	_textVisual.setPosition(sf::Vector2f(_bounds.left + _bounds.width / 2 - _textVisual.getGlobalBounds().width / 2, _bounds.top + _bounds.height / 2 - 20));
+	_textVisual.setString(' ');
+
+	_background.setPosition(sf::Vector2f(_bounds.left + 1, _bounds.top + 1));
+	_background.setFillColor(sf::Color(5, 5, 5));
+	_background.setOutlineThickness(1);
+	_background.setOutlineColor(sf::Color::White);
+
+	_currentState = SolutionState::NO_STATE;
+}
+
+void PuzzleLetter::draw(sf::RenderWindow& renderWindow) const
+{
+	renderWindow.draw(_background);
+	renderWindow.draw(_textVisual);
+}
+
+char PuzzleLetter::getLetter() const
+{
+	return _letter;
+}
+
+void PuzzleLetter::setLetter(const char letter)
+{
+	_letter = letter;
+	_textVisual.setString(letter);
+}
+
+void PuzzleLetter::setSolutionState(const SolutionState& solutionState)
+{
+	_currentState = solutionState;
+	switch (_currentState)
+	{
+		case SolutionState::NO_STATE:
+			_background.setFillColor(sf::Color(40, 40, 40));
+			break;
+		case SolutionState::CORRECT:
+			_background.setFillColor(sf::Color(93, 141, 74));
+			break;
+		case SolutionState::WRONG_POS:
+			_background.setFillColor(sf::Color(141, 141, 74));
+			break;
+	}
+}
+
+PuzzleLetter::SolutionState PuzzleLetter::getSolutionState() const
+{
+	return _currentState;
+}
+
+void PuzzleLetter::setBackgroundColour(const sf::Color& colour)
+{
+	_background.setFillColor(colour);
+}
--- WordleSFML/WordleSFML/PuzzleLetter.h
+++ WordleSFML/WordleSFML/PuzzleLetter.h
@@ -5,8 +5,44 @@
 class PuzzleLetter
 {
 public:
-	
+	// Defines the state for the solution
+	enum SolutionState { NO_STATE, CORRECT, WRONG_POS };
+
+	// Defaults to an empty letter at the position
+	PuzzleLetter(const sf::IntRect& bounds, const sf::Font& font);
+	virtual ~PuzzleLetter() = default;
+
+	// Draws the button
+	void draw(sf::RenderWindow& renderWindow) const;
+
+	// Gets the letter associated
+	char getLetter() const;
+
+	// Sets the letter to the selected letter
+	void setLetter(const char letter);
+
+	// Changes the background colour and stores the solution state
+	void setSolutionState(const SolutionState& solutionState);
+
+	// Get solution state of this element
+	SolutionState getSolutionState() const;
 
 private:
+	// The bounds of the rectangle for isPositionInside()
+	sf::IntRect _bounds;
 
+	// The character shown on the puzzle letter
+	char _letter;
+
+	// Letter background
+	sf::RectangleShape _background;
+
+	// Text Render
+	sf::Text _textVisual;
+
+	// The current solution state of the letter
+	SolutionState _currentState;
+
+	// Changes the background to the selected colour
+	void setBackgroundColour(const sf::Color& colour);
 };
